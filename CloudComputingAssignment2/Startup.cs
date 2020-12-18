3a89fb4809bbd9097e50608ee3f064ae557bcd52
Pranav Bakre <psbakre@yahoo.com>
2020-12-18 10:28:58 +0530

    Empty HTML with default Identity

--- CloudComputingAssignment2/Startup.cs
+++ CloudComputingAssignment2/Startup.cs
@@ -1,6 +1,11 @@
+using CloudComputingAssignment2.Data;
+using CloudComputingAssignment2.Models;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +17,29 @@ namespace CloudComputingAssignment2
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Data.AssignmentDbContext>(options => 
+            options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")
+                ));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<AssignmentDbContext>();
+            services.AddIdentityServer()
+                .AddApiAuthorization<User,AssignmentDbContext>();
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,13 +47,30 @@ namespace CloudComputingAssignment2
         {
             if (env.IsDevelopment())
             {
+                
                 app.UseDeveloperExceptionPage();
+
+                app.UseWebAssemblyDebugging();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseIdentityServer();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
